39eacbdd104f1074dc108b410268846c202daeb0
Vini Moraes <81641780+vinis-moraes@users.noreply.github.com>
2023-06-08 19:25:18 -0300

    Create index.php

---
+++ d013/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Desafio 013</title>
+    <link rel="stylesheet" href="../../style.css">
+</head>
+<body>
+    <h1>Desafio 013</h1>
+    <?php 
+        
+    ?>
+    <form action="<?=$_SERVER['PHP_SELF']?>" method="get">
+
+    </form>
+    <?php 
+
+    ?>
+</body>
+</html>
